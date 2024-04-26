cfe6253bc185123a33709ed8dd3b0cd3c0b6d2b7
iliamarkovski <markovskiilia@gmail.com>
2024-04-26 11:15:31 +0300

    Add constants

---
+++ constants/colors.ts
@@ -0,0 +1,12 @@
+export type LightColorType = 'light';
+export type DarkColorType = 'dark';
+
+type ColorsType = {
+  light: LightColorType;
+  dark: DarkColorType;
+};
+
+export const colors: ColorsType = {
+  light: 'light',
+  dark: 'dark',
+};
