f879dfb2593d1df73f8020e75a8fb75e1182b3e8
iliamarkovski <markovskiilia@gmail.com>
2024-04-26 12:23:24 +0300

    Add BackToHomeButton component

---
+++ components/BackToHomeButton/BackToHomeButton.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Link from 'next/link';
+import styles from './BackToHomeButton.module.scss';
+import { usePathname } from 'next/navigation';
+import { Icon } from '@/components/Icon/Icon';
+
+const BackToHomeButton = () => {
+  const pathname = usePathname();
+
+  if (pathname === '/') {
+    return null;
+  }
+
+  return (
+    <Link href='/' className={styles.button}>
+      <Icon name='back' className={styles.icon} />
+      Back to home
+    </Link>
+  );
+};
+
+export { BackToHomeButton };
