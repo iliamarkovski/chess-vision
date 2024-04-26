afb30c9cdd2d4b5eb3fe81d4fcee725d40d227b0
iliamarkovski <markovskiilia@gmail.com>
2024-04-26 11:15:17 +0300

    Add shuffle util

---
+++ utils/shuffle.ts
@@ -0,0 +1,19 @@
+export const shuffle = <T>(array: T[]): T[] => {
+  const arr = [...array];
+  let currentIndex = arr.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [arr[currentIndex], arr[randomIndex]] = [
+      arr[randomIndex],
+      arr[currentIndex],
+    ];
+  }
+
+  return arr;
+};
