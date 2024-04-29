cd451df76c358b09873090e26734dc286243f368
iliamarkovski <markovskiilia@gmail.com>
2024-04-30 02:43:01 +0300

    Add menu constant

---
+++ constants/menus.ts
@@ -0,0 +1,12 @@
+export const menus = [
+  {
+    href: '/square-color',
+    icon: 'question',
+    title: 'Square Color',
+  },
+  {
+    href: '/knight-moves',
+    icon: 'knight',
+    title: 'Knight Moves',
+  },
+];
